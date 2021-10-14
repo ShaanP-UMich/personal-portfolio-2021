cbe2c80a018cccfe66704878661d11894c58c635
shaanp <shaanp@umich.edu>
2021-10-14 16:23:55 -0400

    Added some starter pages

---
+++ page-portfolio.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+
+<div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h1>This is the Portfolio page</h1>
+</div>
+
+<?php get_footer(); ?>
---
+++ page-test.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+
+<div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h1>This is a test heading on page-test</h1>
+</div>
+
+<?php get_footer(); ?>
